89f5947a6b8c33e21b05b52f9a87bd410e730958
CarlosHSF <44176623+CarlosHSF99@users.noreply.github.com>
2024-03-05 16:55:27 +0000

    Engine with XML parser (#5)
    * Add generator
    
    * Implement sphere and cone generation
    
    * Implement XML parser in engine
    
    * Pass XML file name in stdin
    
    * Move tinyxml2.h to include/deps
    
    * Fix tinyxml2.h imports

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -2,26 +2,93 @@
 #include <GL/glut.h>
 #include <cmath>
 #include "utils/model.h"
+#include "deps/tinyxml2.h"
+
+using namespace tinyxml2;
 
 void renderScene();
+
 void changeSize(int w, int h);
+
+float positionX, positionY, positionZ;
+float lookAtX, lookAtY, lookAtZ;
+float upX, upY, upZ;
+float fov, near, far;
+std::vector<Model> models;
 
 int main(int argc, char **argv)
 {
+    int width, height;
+
+    XMLDocument doc;
+    if (doc.LoadFile(argv[1]) == XML_SUCCESS) {
+        XMLElement *worldElement = doc.FirstChildElement("world");
+        if (worldElement) {
+            XMLElement *windowElement = worldElement->FirstChildElement("window");
+            if (windowElement) {
+                windowElement->QueryIntAttribute("width", &width);
+                windowElement->QueryIntAttribute("height", &height);
+            }
+
+            XMLElement *cameraElement = worldElement->FirstChildElement("camera");
+            if (cameraElement) {
+                XMLElement *positionElement = cameraElement->FirstChildElement("position");
+                if (positionElement) {
+                    positionElement->QueryFloatAttribute("x", &positionX);
+                    positionElement->QueryFloatAttribute("y", &positionY);
+                    positionElement->QueryFloatAttribute("z", &positionZ);
+                }
+                XMLElement *lookAtElement = cameraElement->FirstChildElement("lookAt");
+                if (lookAtElement) {
+                    lookAtElement->QueryFloatAttribute("x", &lookAtX);
+                    lookAtElement->QueryFloatAttribute("y", &lookAtY);
+                    lookAtElement->QueryFloatAttribute("z", &lookAtZ);
+                }
+                XMLElement *upElement = cameraElement->FirstChildElement("up");
+                if (upElement) {
+                    upElement->QueryFloatAttribute("x", &upX);
+                    upElement->QueryFloatAttribute("y", &upY);
+                    upElement->QueryFloatAttribute("z", &upZ);
+                }
+                XMLElement *projectionElement = cameraElement->FirstChildElement("projection");
+                if (projectionElement) {
+                    projectionElement->QueryFloatAttribute("fov", &fov);
+                    projectionElement->QueryFloatAttribute("near", &near);
+                    projectionElement->QueryFloatAttribute("far", &far);
+                }
+            }
+
+            XMLElement *groupElement = worldElement->FirstChildElement("group");
+            if (groupElement) {
+                XMLElement *modelsElement = groupElement->FirstChildElement("models");
+                if (modelsElement) {
+                    XMLElement *modelElement = modelsElement->FirstChildElement("model");
+                    while (modelElement) {
+                        const char *file = modelElement->Attribute("file");
+                        models.emplace_back(file);
+                        modelElement = modelElement->NextSiblingElement("model");
+                    }
+                }
+            }
+        }
+    } else {
+        std::cerr << "Failed to load file." << std::endl;
+    }
+
     // init GLUT and the window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(800,800);
-	glutCreateWindow("CG@DI-UM");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - width) / 2, (glutGet(GLUT_SCREEN_HEIGHT) - height) / 2);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("CG@DI-UM");
 
     // required callback registry
-	glutDisplayFunc(renderScene);
+    glutDisplayFunc(renderScene);
     glutReshapeFunc(changeSize);
 
     // OpenGL settings
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -30,27 +97,30 @@ int main(int argc, char **argv)
     return 1;
 }
 
-void renderScene() {
+void renderScene()
+{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    gluLookAt(10.0, 10.0, 10.0,
-              0.0, 0.0, 0.0,
-              0.0f, 1.0f, 0.0f);
+    gluLookAt(positionX, positionY, positionZ,
+              lookAtX, lookAtY, lookAtZ,
+              upX, upY, upZ);
 
-    Model model("model.3d");
-    model.draw();
+    for (const auto &model: models) {
+        model.draw();
+    }
 
     glutSwapBuffers();
 }
 
-void changeSize(int w, int h) {
+void changeSize(int w, int h)
+{
     // Prevent a divide by zero, when window is too short
     // (you can't make a window of zero width).
     if (h == 0)
         h = 1;
 
-    float ratio = 1.0 * w / h;
+    float ratio = (float) w / (float) h;
 
     // Use the Projection Matrix
     glMatrixMode(GL_PROJECTION);
@@ -62,7 +132,7 @@ void changeSize(int w, int h) {
     glViewport(0, 0, w, h);
 
     // Set the correct perspective.
-    gluPerspective(45, ratio, 1, 1000);
+    gluPerspective(fov, ratio, near, far);
 
     // Get Back to the Modelview
     glMatrixMode(GL_MODELVIEW);
